930fd5d04b731444455627f9ee71e6b4f433e367
xusong <595304523@qq.com>
2018-08-05 12:01:41 +0800

    collection

---
+++ src/main/java/com/xusong/collectionmark1/list/AbstractLinkedList.java
@@ -0,0 +1,9 @@
+package com.xusong.collectionmark1.list;
+
+import java.util.List;
+
+/**
+ * Created by xusong on 2018/7/22.
+ */
+public abstract class AbstractLinkedList<E> implements List<E> {
+}
---
+++ src/main/java/com/xusong/collectionmark1/list/CursorableLinkedList.java
@@ -0,0 +1,7 @@
+package com.xusong.collectionmark1.list;
+
+/**
+ * Created by xusong on 2018/7/22.
+ */
+public class CursorableLinkedList {
+}
